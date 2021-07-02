98f7e534aca67f6b96a4bf2c3252832fdcede696
tanisha <tanishamandre5@gmail.com>
2021-07-02 10:58:07 +0100

    update

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    json_file = {}
+    json_file['query'] = 'hello_worldliness'
+    print("returning hellow world")
+    return jsonify(json_file)
