03c71e3157eff44b3fbb3de94246b2f138dc6890
tanisha <tanishamandre5@gmail.com>
2021-07-05 14:34:49 +0100

    update

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ def index():
     json_file['query'] = 'hello_worldliness'
     print("returning hellow world")
     return jsonify(json_file)
+
+@app.route('/hello')
+def hello():
+    json_file = {}
+    json_file['query'] = 'hellow'
+    return jsonify(json_file)
+
+@app.route('/world')
+def world():
+    json_file = {}
+    json_file['query'] = 'worldy'
+    return jsonify(json_file)
