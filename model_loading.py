e3a45f83adaa22631f3f66c56d06eb30d274a461
tanisha <tanishamandre5@gmail.com>
2021-07-07 10:37:17 +0100

    Model deleted

---
+++ model_loading.py
@@ -0,0 +1,6 @@
+import pickle
+import torch
+
+PATH = 'muss/resources/models/muss_en_wikilarge_mined/model.pt'
+# model = torch.load(PATH)
+print(model)
--- simplification/summariser.py
+++ simplification/summariser.py
@@ -1,6 +1,11 @@
-from simplification.edmundson import edmundsons
-from simplification.muss_simp import simplify_muss
-from simplification.control_simp import controllable_simplification
+# from simplification.edmundson import edmundsons
+# from simplification.muss_simp import simplify_muss
+# from simplification.control_simp import controllable_simplification
+
+from edmundson import edmundsons
+from muss_simp import simplify_muss
+from control_simp import controllable_simplification
+
 from multiprocessing import Process, freeze_support
 
 def summarise_text(text, simplifier="muss", percentage=0.8):
@@ -18,7 +23,5 @@ def summarise_text(text, simplifier="muss", percentage=0.8):
 if __name__ == '__main__':
     freeze_support()
     text = "The Lok Sabha is elected for a term of five years. Its life can be extended for one year at a time during a national emergency. It can be dissolved earlier than its term by the President on the advice of the Prime Minister. It can be voted out of power by a debate and vote on a no-confidence motion. During the 13th Lok Sabha, Bhartiya Janata Party lost a no-confidence motion by one vote and had to resign."
-
-
     print(summarise_text(text))
     
